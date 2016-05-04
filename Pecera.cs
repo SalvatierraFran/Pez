17a9fab1b432100c925adac80cfbbd845f0d5177
SalvatierraFran <franco14_93@hotmail.com>
2016-05-04 02:45:55 -0300

    Final

---
+++ Pecera.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialPez
+{
+    class Pecera
+    {
+        private List<Salvatierra.Franco.Pez> _peces;
+        private Single _volumen;
+        private int _capacidadDePeces;
+        private bool _aguaCaliente;
+
+        private Pecera()
+        {
+            this._peces = new List<Salvatierra.Franco.Pez>(3);
+        }
+
+        public Pecera(Single volumen, bool aguaCaliente):this()
+        {
+            this._volumen = volumen;
+            this._aguaCaliente = aguaCaliente;
+        }
+    }
+}
